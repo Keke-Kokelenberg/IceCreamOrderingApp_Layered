c0c0bbc62c16f450e7094910dce22a6cd9c25c12
Keke-Kokelenberg <keke.kokelenberg@vdabcampus.be>
2025-11-03 15:09:03 +0100

    Commented out `ImageUrl` property in Vendor and IceCream entities.

--- Repositories/Entities/IceCream.cs
+++ Repositories/Entities/IceCream.cs
@@ -8,7 +8,7 @@ public class IceCream
     public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
-    public string? ImageUrl { get; set; }
+    //public string? ImageUrl { get; set; }
     public decimal Price { get; set; }
     
     public int VendorId { get; set; }
